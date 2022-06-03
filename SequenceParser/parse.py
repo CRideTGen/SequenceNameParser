d40165a8c7d68bc2688479b528b58c5656e8ddbf
CRideTGen <cridenour@tgen.org>
2022-06-02 17:22:11 -0700

    implemented the FileParts dataclass with unit tests

--- SequenceParser/parse.py
+++ SequenceParser/parse.py
@@ -1,47 +1,81 @@
-import pathlib
-from dataclasses import dataclass
+import re
+from pathlib import Path
+from dataclasses import dataclass, field
 from typing import Protocol
-# from abc import ABC, abstractmethod
+
+
 @dataclass
 class FileParts:
-    pass
+    prefix: str
+    sample_name: str
+    s_part: str
+    read_direction: str
+    suffix: str
+
+    def __init__(self, filename):
+        self.prefix = self.get_prefix(filename)
+        self.sample_name = self.get_sample_name(filename)
+        self.s_part = self.get_s_part(filename)
+        self.read_direction = self.get_read_direction(filename)
+        self.suffix = self.get_suffix(filename)
+
+    def get_prefix(self, filename: str) -> str:
+        match = re.search(r"(.*-)[0-9A-Za-z]_", filename)
+        return match.group(1)
+
+    def get_sample_name(self, filename: str) -> str:
+        match = re.search(r"[0-9A-Za-z]_.*M[2,3]", filename)
+        return match.group(0)
+
+    def get_s_part(self, filename: str) -> str:
+        match = re.search(r"_S[0-9]{1,3}_", filename)
+        return match.group(0)
+
+    def get_read_direction(self, filename: str) -> str:
+        match = re.search(r"_(R[1,2])_", filename)
+        return match.group(1)
+
+    def get_suffix(self, filename: str) -> str:
+        match = re.search(r"_001[.].*", filename)
+        return match.group(0)
+
 
 class FileType(Protocol):
-    file_dir: pathlib.Path
-    file_names: list[str]
-    filename_parts: dict
+    file_dir: Path
+    file_names: dict[str, list[str]]
+    filename_parts: list[FileParts]
 
     def gather_file_names(self, keyword: str) -> None:
         ...
 
+    def parse_file_names(self):
+        ...
 
-class PairedEndIllumina(FileType):
+
+class PairedEndIllumina:
     def __init__(self, file_path: str) -> None:
-        self.file_dir = pathlib.Path(file_path)
+        self.file_dir = Path(file_path)
 
         if not self.file_dir.is_dir():
             raise ValueError(f"{self.file_dir} is not a valid directory.")
 
-        self.file_names = []
-        self.filename_parts = dict()
+        self.file_names = dict()
+        self.file_parts = list()
 
     def gather_file_names(self, keyword: str = None) -> None:
         if keyword:
-            file_names = self.file_dir.glob(f"*{keyword}*fastq*")
+            forward_reads = self.file_dir.glob(f"*{keyword}*R1*fastq*")
+            reverse_reads = self.file_dir.glob(f"*{keyword}*R2*fastq*")
         else:
-            file_names = self.file_dir.glob(f"*.fastq*")
+            forward_reads = self.file_dir.glob(f"*R1*fastq*")
+            reverse_reads = self.file_dir.glob(f"*R2*fastq*")
 
-        self.file_names = [file.name for file in file_names]
+        self.file_names["forward_reads"] = [file.name for file in forward_reads]
+        self.file_names["reverse_reads"] = [file.name for file in reverse_reads]
 
-        if not self.file_names:
+        if not self.file_names["forward_reads"] and not self.file_names["reverse_reads"]:
             raise ValueError(f"{self.file_dir} is empty.")
 
+    def parse_file_names(self):
 
-def parser_illumina_sdsi(parser: FileType) -> dict:
-    parser.gather_file_names("SDSI")
-
-
-
-if __name__ == "__main__":
-    t_filetype = PairedEndIllumina("weird")
-    parser_illumina_sdsi(parser=t_filetype)
+        pass
--- SequenceParser/tests/test_parse.py
+++ SequenceParser/tests/test_parse.py
@@ -1,14 +1,17 @@
 import pytest
-from SequenceParser.parse import PairedEndIllumina
+from SequenceParser.parse import PairedEndIllumina, FileParts
 
 
 @pytest.fixture(scope="session")
 def sequence_dir(tmp_path_factory):
     fn = tmp_path_factory.mktemp("data")
-    (fn / "test.fastq").touch()
-    (fn / "weird_key.fastq").touch()
-    (fn / "weird.fastq").touch()
-    [print(x) for x in fn.glob("*")]
+    (fn / "test_R1_001.fastq").touch()
+    (fn / "weird_key_R1_001.fastq").touch()
+    (fn / "weird_R1_001.fastq").touch()
+    (fn / "test_R2_001.fastq").touch()
+    (fn / "weird_key_R2_001.fastq").touch()
+    (fn / "weird_R2_001.fastq").touch()
+
     return str(fn)
 
 
@@ -21,13 +24,17 @@ def empty_dir(tmp_path_factory):
 def test_gather_file_names_no_key(sequence_dir):
     tmp_class = PairedEndIllumina(sequence_dir)
     tmp_class.gather_file_names()
-    assert tmp_class.file_names == ["test.fastq", "weird_key.fastq", "weird.fastq"]
+    assert tmp_class.file_names["forward_reads"] == ["test_R1_001.fastq", "weird_key_R1_001.fastq",
+                                                     "weird_R1_001.fastq"]
+    assert tmp_class.file_names["reverse_reads"] == ["test_R2_001.fastq", "weird_key_R2_001.fastq",
+                                                     "weird_R2_001.fastq"]
 
 
 def test_gather_file_names_with_key(sequence_dir):
     tmp_class = PairedEndIllumina(sequence_dir)
     tmp_class.gather_file_names(keyword="key")
-    assert tmp_class.file_names == ["weird_key.fastq"]
+    assert tmp_class.file_names["forward_reads"] == ["weird_key_R1_001.fastq"]
+    assert tmp_class.file_names["reverse_reads"] == ["weird_key_R2_001.fastq"]
 
 
 def test_gather_file_names_empty_dir(empty_dir):
@@ -39,3 +46,32 @@ def test_gather_file_names_empty_dir(empty_dir):
 def test_paired_end_illumina():
     with pytest.raises(ValueError) as excinfo:
         PairedEndIllumina("/path/to/not_a_dir")
+
+
+test_filename = "TGen-CoV-AZ-Tiled-C_SDSI_M2_S265_R2_001.fastq"
+test_prefix = "TGen-CoV-AZ-Tiled-"
+test_sample_name = "C_SDSI_M2"
+test_s_part = "_S265_"
+test_read_direction = "R2"
+test_suffix = "_001.fastq"
+
+test_fileparts = FileParts(test_filename)
+
+def test_get_prefix():
+    assert test_fileparts.prefix == test_prefix
+
+
+def test_get_sample_name():
+    assert test_fileparts.sample_name == test_sample_name
+
+
+def test_get_s_part():
+    assert test_fileparts.s_part == test_s_part
+
+
+def test_get_read_direction():
+    assert test_fileparts.read_direction == test_read_direction
+
+
+def test_get_suffix():
+    assert test_fileparts.suffix == test_suffix
